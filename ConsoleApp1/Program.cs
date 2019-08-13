d5a7cccc62218bf773ad0e9aa6374ea18096cbb0
unknown <Ing@UA12464.ua.cl>
2019-08-13 09:25:40 -0400

    Clase 2

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("hola");
+            //int i = 0;
+            ////python while i<10:
+            //while (i<10)
+            //// i++ = i+=1 
+            //{  if (i < 3)
+            //   {
+            //        Console.WriteLine("soy menor que 3");
+            //   }
+            //    else if (i >= 3 && i <= 7) { Console.WriteLine("estoy entre 3 y 7"); }
+            //    else { Console.WriteLine("estoy entrer 8 y 9"); }
+            //Console.WriteLine(i);
+            //i++;
+            //}
+            List<int> myIntList;
+            myIntList = new List<int>() { 1, 2, 5, -10 };
+
+            //for (int j = 0; j < myIntList.Count; j++)
+            //{
+            //    Console.WriteLine(myIntList[j]);
+            //}
+
+            int caseSwitch = 1;
+            //switch es en vez de un if
+            switch(caseSwitch){
+                case 1:
+                    Console.WriteLine("case 1");
+                    break;
+                case 2:
+                    Console.WriteLine("case 2");
+                    break;
+            }
+        }
+    }
+}
